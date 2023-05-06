174143a98a8e84b168aac21bb877047add64c28a
Tien Nguyen <nguyenthanhtien@icloud.com>
2023-05-06 12:13:42 +0700

    Init project

---
+++ api/SaaS.ServiceInterface/MyServices.cs
@@ -0,0 +1,14 @@
+using System;
+using ServiceStack;
+using SaaS.ServiceModel;
+
+namespace SaaS.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
